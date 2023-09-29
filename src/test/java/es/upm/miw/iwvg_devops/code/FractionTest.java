29bf7970c6dda0946a027c9001327745953140b1
jiaan <chengjiaan123@gmail.com>
2023-09-29 19:57:55 +0200

    Added more test in FractionTest class #27

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -37,4 +37,52 @@ class FractionTest {
         assertEquals(0.5, fraction.decimal());
     }
 
+    @Test
+    void testFractionIsProper(){
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    void testFractionIsImproper(){
+        assertFalse(fraction.isImproper());
+    }
+
+    @Test
+    void testFractionSum(){
+        Fraction fraction = new Fraction(1,4);
+        Fraction espectedResultFraction = new Fraction(3,4);
+        Fraction resultFraction = this.fraction.add(fraction);
+        assertEquals(espectedResultFraction.getDenominator(), resultFraction.getDenominator());
+        assertEquals(espectedResultFraction.getNumerator(), resultFraction.getNumerator());
+    }
+
+    @Test
+    void testFractionIsEquivalent(){
+        Fraction fraction = new Fraction(2,4);
+        assertTrue(fraction.isEquivalent(fraction));
+    }
+
+    @Test
+    void testFractionMultiply(){
+        Fraction fraction = new Fraction(1,2);
+        Fraction espectedResultFraction = new Fraction(2,8);
+        Fraction resultFraction = this.fraction.multiply(fraction);
+        assertEquals(espectedResultFraction.getDenominator(), resultFraction.getDenominator());
+        assertEquals(espectedResultFraction.getNumerator(), resultFraction.getNumerator());
+    }
+
+    @Test
+    void testFractionDivide(){
+        Fraction fraction = new Fraction(1,2);
+        Fraction espectedResultFraction = new Fraction(4,4);
+        Fraction resultFraction = this.fraction.divide(fraction);
+        assertEquals(resultFraction.getDenominator(), espectedResultFraction.getDenominator());
+        assertEquals(resultFraction.getNumerator(), espectedResultFraction.getNumerator());
+    }
+
+    @Test
+    void testFractionToString(){
+        assertEquals("Fraction{numerator=2, denominator=4}", fraction.toString());
+    }
+
 }
